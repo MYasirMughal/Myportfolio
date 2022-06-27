e07d01a27943e9840b270ebce2541fc6f98a6d1d
frans <franswinata6@gmail.com>
2022-06-27 11:41:12 +0700

    Work section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,52 +8,38 @@ import FireBase from '../assets/firebase.png';
 import AWS from '../assets/aws.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+import MongoDB from '../assets/mongo.png';
 
 const Skills = () => {
+    function gridItem(a, b) {
+        return (
+            <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
+                    <img className='w-20 mx-auto' src={a} alt={b} />
+                    <p className='mt-4'>{b}</p>
+            </div>
+        )
+    }
+
   return (
-    <div name='skills' className='w-full h-full bg-[#0A192F] text-gray-300 px-4'>
+    <div name='skills' className='w-full h-full md:h-screen bg-[#0A192F] text-gray-300 px-4'>
 
         {/* Container */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='mt-[100px]'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
                 <p className='py-4'>// There are the tech stacks I've worked with</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={HTML} alt="HTML" />
-                    <p className='mt-4'>HTML</p>
-                </div>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={CSS} alt="CSS" />
-                    <p className='mt-4'>CSS</p>
-                </div>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript" />
-                    <p className='mt-4'>JavaScript</p>
-                </div>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={ReactImg} alt="React" />
-                    <p className='mt-4'>React</p>
-                </div>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind" />
-                    <p className='mt-4'>Tailwind CSS</p>
-                </div>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={GitHub} alt="GitHub" />
-                    <p className='mt-4'>GitHub</p>
-                </div>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={Node} alt="NodeJS" />
-                    <p className='mt-4'>NodeJS</p>
-                </div>
-                <div className='pt-6 pb-4 py-4 rounded-lg shadow-none hover:shadow-lg hover:shadow-pink-600 hover:scale-110 duration-500 hover:cursor-pointer'>
-                    <img className='w-20 mx-auto' src={Mongo} alt="Mongo" />
-                    <p className='mt-4'>MongoDB</p>
-                </div>
+                {gridItem(HTML, "HTML")}
+                {gridItem(CSS, "CSS")}
+                {gridItem(JavaScript, "JavaScript")}
+                {gridItem(ReactImg, "React")}
+                {gridItem(Tailwind, "Tailwind CSS")}
+                {gridItem(GitHub, "GitHub")}
+                {gridItem(Node, "NodeJS")}
+                {gridItem(MongoDB, "MongoDB")}
+
             </div>
         </div>
 
---
+++ src/components/Work.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import WorkImg from '../assets/workImg.jpeg'
+import realEstate from '../assets/realestate.jpg'
+
+const Work = () => {
+    function gridItem(a, b, c) {
+        return (
+            <div
+                style={{backgroundImage: `url(${a})`}}
+                className='shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            >
+
+                {/* Hover Effect */}
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                        React JS Application
+                    </span>
+                    <div className='pt-8 text-center '>
+                        <a href={b ? b : '/'}>
+                            <button className='w-20 text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href={c ? c : '/'}>
+                            <button className='w-20 text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0A192F]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Projects</p>
+                <p className='py-4'>// Check out some of my favourite projects</p>
+            </div>
+
+            {/* Container */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                {/* Grid */}
+                {gridItem(
+                    WorkImg,
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx",
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx"
+                )}
+                {gridItem(
+                    realEstate,
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx",
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx"
+                )}
+                {gridItem(
+                    WorkImg,
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx",
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx"
+                )}
+                {gridItem(
+                    realEstate,
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx",
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx"
+                )}
+                {gridItem(
+                    WorkImg,
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx",
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx"
+                )}
+                {gridItem(
+                    realEstate,
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx",
+                    "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/Skills.jsx"
+                )}
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
