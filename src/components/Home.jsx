a9e727f595c78bb87973aabb7cba6a9b0763a7ba
frans <franswinata6@gmail.com>
2022-06-29 12:06:01 +0700

    remove sass

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -12,7 +13,7 @@ const Home = () => {
             <h2 className='text-3xl sm:text-6xl font-bold text-gray-400'>I'm a React Native Developer.</h2>
             <p className='text-gray-400 py-4 max-w-[700px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <div>
-                <button className='text-white border-2 px-6 py-3 my-2 flex items-center gap-2 hover:bg-pink-600 hover:border-pink-600 duration-200 hover:gap-5'>View Work <HiArrowNarrowRight /></button>
+                <Link to="/work" className='w-[175px] text-white border-2 px-6 py-3 my-2 flex justify-center items-center gap-2 hover:bg-pink-600 hover:border-pink-600 duration-200 hover:gap-5'>View Work <HiArrowNarrowRight /></Link>
             </div>
         </div>
 
--- src/workData.js
+++ src/workData.js
@@ -18,22 +18,10 @@ let data = [
         code: "https://github.com/fransachmadhw/DreadOut2_Website"
     },
     {
-        name: "Blogshot",
-        img: "https://repository-images.githubusercontent.com/452623686/9ea14ce5-e13f-43f6-860c-96fdd0df6de2",
+        name: "React Portfolio 1",
+        img: "https://repository-images.githubusercontent.com/505251852/e35f6a78-a7a5-4277-836c-143b7b003111",
         live: "https://blogshot.vercel.app",
-        code: "https://github.com/fransachmadhw/Blogshot"
-    },
-    {
-        name: "Trivia React",
-        img: "https://repository-images.githubusercontent.com/501931143/ed1599a9-2297-40a5-811f-dddf47c24e22",
-        live: "https://triviaaa.vercel.app",
-        code: "https://github.com/fransachmadhw/Trivia-React"
-    },
-    {
-        name: "DreadOut 2 Unofficial Website Remake",
-        img: "https://repository-images.githubusercontent.com/431815358/fac7e016-1285-4399-afc9-d1f252b28dca",
-        live: "https://dreadout2unofficial.netlify.app",
-        code: "https://github.com/fransachmadhw/DreadOut2_Website"
+        code: "https://github.com/fransachmadhw/react-portfolio-1"
     },
 ]
 
