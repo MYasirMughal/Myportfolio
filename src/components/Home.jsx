1d74368496cec83aebb10a0ac76930d63ffba7d8
MYasirMughal <144555237+MYasirMughal@users.noreply.github.com>
2024-10-09 21:52:40 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
-import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -11,8 +10,7 @@ const Home = () => {
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-white'Muhammad Yasir</h1>
             <h2 className='text-3xl sm:text-6xl font-bold text-gray-400'>I'm Front End Developer.</h2>
-            <p className='text-gray-400 py-4 max-w-[700px]'>A passionate and detail-oriented front-end developer with a strong background in creating visually appealing and responsive user interfaces. Proficient in HTML, CSS, and JavaScript, with experience using modern front-end frameworks like React, Vue.js, or Angular. Skilled at transforming designs into functional web applications that provide a seamless user experience across devices. Strong understanding of UX/UI principles, version control (Git), and cross-browser compatibility. Continuously learning and keeping up with industry trends to deliver optimized, high-performance web solutions. Collaborative team player with excellent problem-solving abilities.
-</p>
+            <p className='text-gray-400 py-4 max-w-[700px]'>A passionate and detail-oriented front-end developer with a strong background in creating visually appealing and responsive user interfaces. Proficient in HTML, CSS, and JavaScript, with experience using modern front-end frameworks like React, Vue.js, or Angular. Skilled at transforming designs into functional web applications that provide a seamless user experience across devices. Strong understanding of UX/UI principles, version control (Git), and cross-browser compatibility. Continuously learning and keeping up with industry trends to deliver optimized, high-performance web solutions. Collaborative team player with excellent problem-solving abilities.</p>
            
         </div>
 
