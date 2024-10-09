1f43cc773e33a3d2932a5922dc91cec66d6303d3
MYasirMughal <144555237+MYasirMughal@users.noreply.github.com>
2024-10-09 21:51:47 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
         <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-center items-center ml-[-100px] hover:ml-[0px] duration-300 bg-blue-600'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/m-yasir-mughal/">LinkedIn <M Yasir size={30} /></a>
+                    <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/m-yasir-mughal/">LinkedIn <Yasir size={30} /></a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-center items-center ml-[-100px] hover:ml-[0px] duration-300 bg-gray-800'>
                     <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/myasirmughal">GitHub <FaGithub size={30} /></a>
